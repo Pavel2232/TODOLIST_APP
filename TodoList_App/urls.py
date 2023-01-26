0e6b3d6142b3ccd4c1f564c28c0ecad495488f57
Павел <king-he@mail.ru>
2023-01-26 16:48:15 +0300

    initall+

--- TodoList_App/urls.py
+++ TodoList_App/urls.py
@@ -1,4 +1,4 @@
-"""TODOLIST_APP URL Configuration
+"""TodoList_App URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
